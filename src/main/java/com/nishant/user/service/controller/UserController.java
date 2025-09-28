443fc7ab710a31669c6f0714bf8f6a686385ef5b
shui6540 <shubhi060504@gmail.com>
2025-09-28 20:54:12 +0530

    Updated userController

--- src/main/java/com/nishant/user/service/controller/UserController.java
+++ src/main/java/com/nishant/user/service/controller/UserController.java
@@ -6,23 +6,73 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
+//@RestController
+//public class UserController {
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @PostMapping("/api/users")
+//    public User createUser(User user){
+//        return userRepository.save(user);
+//    }
+//    @GetMapping("/api/users")
+//    public User getUser() {
+//        User user = new User();
+//        user.setEmail("nishant@gmail.com");
+//        user.setFullName("Nishant Gaur");
+//        user.setPhone("+91 9098765678");
+//        user.setRole("Customer");
+//        return user;
+//    }
+//}
 @RestController
 public class UserController {
+
     @Autowired
     private UserRepository userRepository;
 
     @PostMapping("/api/users")
-    public User createUser(User user){
+    public User createUser(@RequestBody User user){
         return userRepository.save(user);
     }
     @GetMapping("/api/users")
-    public User getUser() {
-        User user = new User();
-        user.setEmail("nishant@gmail.com");
-        user.setFullName("Nishant Gaur");
-        user.setPhone("+91 9098765678");
-        user.setRole("Customer");
-        return user;
+    public List<User> getUsers(){
+        return userRepository.findAll();
+    }
+    @GetMapping("api/users/{id}")
+    public User getUserById(@PathVariable Long id) throws Exception {
+        Optional<User>otp=userRepository.findById(id);
+        if(otp.isPresent()){
+            return otp.get();
+        }
+        throw new Exception("user not found");
+    }
+
+    @PutMapping("/api/users/{id}")
+    public User updateUser(@RequestBody User user, @PathVariable Long id) throws Exception {
+        Optional<User>otp=userRepository.findById(id);
+        if(otp.isEmpty()){
+            throw new Exception("user not found with id"+id);
+        }
+        User existingUser=otp.get();
+        existingUser.setFullName(user.getFullName());
+        existingUser.setEmail(user.getEmail());
+        existingUser.setPhone(user.getPhone());
+        existingUser.setRole(user.getRole());
+
+        return userRepository.save(existingUser);
+    }
+
+
+    @DeleteMapping("api/users/{id}")
+    public String deleteUserById(@PathVariable Long id) throws Exception {
+        Optional<User>otp=userRepository.findById(id);
+        if(otp.isEmpty()){
+            throw new Exception("user not found with id"+id);
+        }
+        userRepository.deleteById(otp.get().getId());
+        return "User deleted successfully";
     }
 }
