b88ce7eff6709a38a9583073af9fbdd48301f9db
Nishant <118586587+nishant1810@users.noreply.github.com>
2025-10-03 00:43:55 +0530

    Add files via upload

---
+++ salon-service/src/main/java/com/nishant/salon/service/mapper/SalonMapper.java
@@ -0,0 +1,25 @@
+package com.nishant.salon.service.mapper;
+
+
+import com.nishant.salon.service.modal.Salon;
+import com.nishant.salon.service.payload.DTO.SalonDTO;
+
+public class SalonMapper {
+
+    public static SalonDTO mapToDTO(Salon salon) {
+        SalonDTO salonDTO = new SalonDTO();
+
+        salonDTO.setId(salon.getId());
+        salonDTO.setName(salon.getName());
+        salonDTO.setOwnerId(salon.getOwnerId());
+        salonDTO.setImages(salon.getImages());
+//        salonDTO.setEmail(salon.getEmail());
+        salonDTO.setAddress(salon.getAddress());
+        salonDTO.setPhoneNumber(salon.getPhoneNumber());
+        salonDTO.setOpenTime(salon.getOpenTime());
+        salonDTO.setCloseTime(salon.getCloseTime());
+        salonDTO.setCity(salon.getCity());
+
+        return salonDTO;
+    }
+}
