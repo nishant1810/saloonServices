cea63ca2068cf6ed646ec8b3ce56489ffe507640
Nishant <118586587+nishant1810@users.noreply.github.com>
2025-10-03 22:11:21 +0530

    Add files via upload

---
+++ payment-service/src/main/java/com/nishant/payment/service/repository/SalonRepository.java
@@ -0,0 +1,20 @@
+package com.nishant.salon.service.repository;
+
+import com.nishant.salon.service.modal.Salon;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface SalonRepository extends JpaRepository<Salon, Long> {
+
+    Salon findByOwnerId(long id);
+
+    @Query("SELECT s FROM Salon s WHERE " +
+            "LOWER(s.city) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(s.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(s.address) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Salon> searchSalons(@Param("keyword") String keyword);
+}
+
